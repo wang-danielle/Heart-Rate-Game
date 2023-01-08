415b64ac60a6b4b8208c7ed250fe9242ea6c3501
Danielle Wang <danielle.wang@epfl.ch>
2023-01-09 00:14:37 +0100

    Require exercises in custom level creation

--- HeartRateGame/mobile/src/main/java/com/example/heartrategame/CustomLevelFragment.kt
+++ HeartRateGame/mobile/src/main/java/com/example/heartrategame/CustomLevelFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
@@ -77,6 +78,14 @@ class CustomLevelFragment : Fragment() {
         })
 
         binding.createButton.setOnClickListener {
+            if (sharedViewModel.levelData.value?.exercises?.isEmpty() == true) {
+                Toast.makeText(
+                    context,
+                    "You must add exercises!",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
             sharedViewModel.saveLevel(binding.levelNameEditText.text.toString())
             view?.findNavController()?.popBackStack()
             sharedViewModel.resetLevel()
