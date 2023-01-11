d371c4f2ea03e6e9b795823d4724b5f0d27537d8
Danielle Wang <danielle.wang@epfl.ch>
2023-01-11 01:37:14 +0100

    Add more exercise types

--- HeartRateGame/mobile/src/main/java/com/example/heartrategame/models/Exercise.kt
+++ HeartRateGame/mobile/src/main/java/com/example/heartrategame/models/Exercise.kt
@@ -3,4 +3,8 @@ package com.example.heartrategame.models
 enum class Exercise(val title: String, val imageResource: Int) {
     RUNNING("Running", com.example.heartrategame.R.drawable.running),
     JUMPING("Jumping", com.example.heartrategame.R.drawable.jumping),
+    WALKING("Walking", com.example.heartrategame.R.drawable.walking),
+    DANCING("Dancing", com.example.heartrategame.R.drawable.dancing),
+    CYCLING("Cycling", com.example.heartrategame.R.drawable.cycling),
+    SWIMMING("Swimming", com.example.heartrategame.R.drawable.swimming),
 }
